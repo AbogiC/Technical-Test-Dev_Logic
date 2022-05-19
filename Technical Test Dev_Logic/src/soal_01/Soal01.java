07caca20ce00050c23701d8e21e93459b6a27595
Abednego Bogi Christian <99196129+AbogiC@users.noreply.github.com>
2022-05-19 13:43:26 +0700

    add soal 1-3

---
+++ Technical Test Dev_Logic/src/soal_01/Soal01.java
@@ -0,0 +1,21 @@
+package soal_01;
+
+import java.util.Scanner;
+
+public class Soal01 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Masukkan nilai n: ");
+		int n = input.nextInt();
+		for(int i = 0; i < n; i++) {
+			if((i+1)%3==0) {
+				System.out.print("OK ");
+			} else if((i+1)%4==0) {
+				System.out.print("YES ");
+			} else {
+				System.out.print((i+1)+" ");
+			}
+		}
+		input.close();
+	}
+}
---
+++ Technical Test Dev_Logic/src/soal_02/Soal02.java
@@ -0,0 +1,43 @@
+package soal_02;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Soal02 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Masukkan nilai n: ");
+		int n = input.nextInt();
+		//case 1
+//		for(int i = 0; i < n; i++){
+//			for(int j = 0; j <= i; j++){
+//				System.out.print(i+1);
+//			}
+//			System.out.println();
+//		}
+		
+		//case 2
+//		for(int i = 0; i < n; i++){
+//			for(int j = i+1; j > 0; j--){
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+		
+		//case 3
+		
+		
+		//case 4
+		int a = 1;
+		int arr[][] = new int[n][n];
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				arr[j][i]=a;
+				a++;
+			}
+		}
+		System.out.print(arr[0][4]);
+		input.close();
+	}
+
+}
