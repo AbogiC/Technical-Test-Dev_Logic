26b99d6087ef389834fad963db8309a596675d7d
Abednego Bogi Christian <99196129+AbogiC@users.noreply.github.com>
2022-05-24 12:30:58 +0700

    edit soal 2

--- Soal1-3/Soal1-3/src/soal2/Soal2.java
+++ Soal1-3/Soal1-3/src/soal2/Soal2.java
@@ -1,6 +1,5 @@
 package soal2;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Soal2 {
@@ -9,34 +8,62 @@ public class Soal2 {
 		System.out.println("Masukkan nilai n: ");
 		int n = input.nextInt();
 		//case 1
-//		for(int i = 0; i < n; i++){
-//			for(int j = 0; j <= i; j++){
-//				System.out.print(i+1);
-//			}
-//			System.out.println();
-//		}
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j <= i; j++){
+				System.out.print((i+1) + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
 		
 		//case 2
-//		for(int i = 0; i < n; i++){
-//			for(int j = i+1; j > 0; j--){
-//				System.out.print(j);
-//			}
-//			System.out.println();
-//		}
+		for(int i = 0; i < n; i++){
+			for(int j = i+1; j > 0; j--){
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
 		
 		//case 3
-		
+		int o = 1;
+		int x = 1;
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j <= i; j++) {
+				System.out.print(o + " ");
+				o = o + x;
+				if(o == 5 || o == 1) {
+					x = x*(-1);
+				}
+			}
+
+			System.out.println();
+		}
+		System.out.println();
 		
 		//case 4
-		int a = 1;
+		int k = 1;
+		int l = 1 + n; 
 		int arr[][] = new int[n][n];
 		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n; j++) {
-				arr[j][i]=a;
-				a++;
+			if(i%2==0) {
+				for(int j = 0; j < n; j++) {
+					arr[j][i] = k++;
+				}
+				k = k + n;
+			} else {
+				for(int j = n-1; j >=0; j--) {
+					arr[j][i] = l++;
+				}
+				l = l + n;
 			}
 		}
-		System.out.print(Arrays.toString(arr));
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
 		input.close();
 	}
 
